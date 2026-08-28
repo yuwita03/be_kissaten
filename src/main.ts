4b6badab68dba793b32e2cc74d02702959206cd2
unknown <sanhiyo261@gmail.com>
2026-08-28 16:36:03 +0700

    fix cors and env config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // sesuaikan port Vite kamu
+    origin: true, // sesuaikan port Vite kamu
     credentials: true,
   });
 
