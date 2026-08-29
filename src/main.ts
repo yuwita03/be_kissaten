e37f7ed6cf9e853a5c96ed8f427207021f327492
unknown <sanhiyo261@gmail.com>
2026-08-29 18:38:15 +0700

    security: add helmet for security headers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [`'self'`],
+          imgSrc: [`'self'`, 'data:', 'https://images.unsplash.com', 'https://res.cloudinary.com'],
+          mediaSrc: [`'self'`, 'https://res.cloudinary.com'],
+          scriptSrc: [`'self'`],
+          connectSrc: [`'self'`, 'https://app.sandbox.midtrans.com'],
+        },
+      },
+    }),
+  );
+
+  app.use((req, res, next) => {
+    res.setHeader('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
+    next();
+  });
 
   app.enableCors({
     origin: true, // sesuaikan port Vite kamu
